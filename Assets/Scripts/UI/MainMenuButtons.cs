6df5d2c8ba38144d1cac5dc6ff2074e205f4906c
Kitsune03 <arnaum03@gmail.com>
2022-12-10 11:44:48 +0100

    MainMenu update
    Controls and credits

--- Assets/Scripts/UI/MainMenuButtons.cs
+++ Assets/Scripts/UI/MainMenuButtons.cs
@@ -5,6 +5,12 @@ public class MainMenuButtons : MonoBehaviour
 	[SerializeField] private string levelToLoad;
 	[SerializeField] private SceneFader sceneFader;
 
+	[SerializeField] private GameObject menuTurret;
+	[SerializeField] private GameObject optionsCanvas;
+	[SerializeField] private GameObject creditsCanvas;
+	private bool isOptionsOpen;
+	private bool isCreditsOpen;
+
 	private void Start()
 	{
 		if (levelToLoad == "") { levelToLoad = "LevelSelect"; }
@@ -15,7 +21,32 @@ public class MainMenuButtons : MonoBehaviour
 		sceneFader.FadeTo(levelToLoad);
 	}
 
-	public void Quit()
+    public void Options()
+    {
+		isOptionsOpen=!isOptionsOpen;
+		if (isOptionsOpen) 
+		{ 
+			menuTurret.SetActive(false); optionsCanvas.SetActive(true);
+
+            //Close Credits if opened
+            if (isCreditsOpen) { creditsCanvas.SetActive(false); isCreditsOpen = false; }
+        }
+		else { menuTurret.SetActive(true); optionsCanvas.SetActive(false); }
+    }
+	public void Credits()
+    {
+        isCreditsOpen = !isCreditsOpen;
+		if (isCreditsOpen) 
+		{
+			menuTurret.SetActive(false); creditsCanvas.SetActive(true);
+
+            //Close Options if opened
+            if (isOptionsOpen) { optionsCanvas.SetActive(false); isOptionsOpen = false; }
+        }
+		else { menuTurret.SetActive(true); creditsCanvas.SetActive(false); }
+    }
+
+    public void Quit()
 	{
 		Debug.Log("gusbai");
 		Application.Quit();
